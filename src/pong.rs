fdee26a88071ebbed669b3c6ca2cba7eb89f49c6
Matt Porter <matt_porter@hotmail.co.uk>
2014-10-16 20:37:30 +0100

    Some movement

--- src/pong.rs
+++ src/pong.rs
@@ -4,34 +4,21 @@ extern crate native;
 use sdl2::video::{Window, PosCentered, OPENGL};
 use sdl2::event::{QuitEvent, poll_event};
 use sdl2::rect::{Rect};
+use sdl2::timer::{delay};
 
 struct Position {
     x: i32,
-    y: i32
+    y: i32,
+    dx: i32,
+    dy: i32
 }
 
-fn main() {
-    // initialize our position variables
-    let l_bat_pos = Position { x: 0, y: 240-60 };
-    let r_bat_pos = Position { x: 640-30, y: 240-60 };
-    let ball_pos  = Position { x: 320-10, y: 240-10 };
+fn update(pos: Position) -> Position {
+    Position { x: pos.x + pos.dx, y: pos.y + pos.dy, dx: pos.dx, dy: pos.dy }
+}
 
-    // start sdl2 with everything
-    sdl2::init(sdl2::INIT_EVERYTHING);
-
-    // Create a window
-    let window  = match Window::new("Pong Game", PosCentered, PosCentered, 640, 480, OPENGL) {
-        Ok(window) => window,
-        Err(err)   => fail!("failed to create window: {}", err)
-    };
-
-    // Create a rendering context
-    let renderer = match sdl2::render::Renderer::from_window(window, sdl2::render::DriverAuto, sdl2::render::ACCELERATED) {
-        Ok(renderer) => renderer,
-        Err(err) => fail!("failed to create renderer: {}", err)
-    };
-
-    // Set the drawing color to a light blue.
+fn render(renderer: &sdl2::render::Renderer<Window>, l_bat_pos: Position, r_bat_pos: Position, ball_pos: Position) {
+        // Set the drawing csdl2::render::Rendererolor to a light blue.
     let _ = renderer.set_draw_color(sdl2::pixels::RGB(101, 208, 246));
 
     // Clear the buffer, using the light blue color set above.
@@ -63,12 +50,42 @@ fn main() {
 
     // Swap our buffer for the present buffer, displaying it.
     let _ = renderer.present();
+}
+
+fn main() {
+    // initialize our position variables
+    let mut l_bat_pos = Position { x: 0, y: 240-60, dx: 0, dy: 0 };
+    let mut r_bat_pos = Position { x: 640-30, y: 240-60, dx: 0, dy: 0 };
+    let mut ball_pos  = Position { x: 320-10, y: 240-10, dx: 1, dy: 1 };
+
+    // start sdl2 with everything
+    sdl2::init(sdl2::INIT_EVERYTHING);
+
+    // Create a window
+    let window  = match Window::new("Pong Game", PosCentered, PosCentered, 640, 480, OPENGL) {
+        Ok(window) => window,
+        Err(err)   => fail!("failed to create window: {}", err)
+    };
+
+    // Create a rendering context
+    let renderer = match sdl2::render::Renderer::from_window(window, sdl2::render::DriverAuto, sdl2::render::ACCELERATED) {
+        Ok(renderer) => renderer,
+        Err(err) => fail!("failed to create renderer: {}", err)
+    };
+
+    render(&renderer, l_bat_pos, r_bat_pos, ball_pos);
 
     // loop until we receive a QuitEvent
     'event : loop {
         match poll_event() {
             QuitEvent(_) => break 'event,
-            _            => continue
+            _            => {
+                l_bat_pos = update(l_bat_pos);
+                r_bat_pos = update(r_bat_pos);
+                ball_pos  = update(ball_pos);
+                render(&renderer, l_bat_pos, r_bat_pos, ball_pos);
+                delay(16);
+            }
         }
     }
 
