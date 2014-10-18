73f54b77ab935a5f5707948c39ff52172dee1d99
Matt Porter <matt_porter@hotmail.co.uk>
2014-10-18 17:09:53 +0100

    Basic collision detection for ball

--- src/pong.rs
+++ src/pong.rs
@@ -4,16 +4,17 @@ extern crate native;
 use sdl2::video::{Window, PosCentered, OPENGL};
 use sdl2::event::{QuitEvent, poll_event};
 use sdl2::rect::{Rect};
-use sdl2::timer::{delay};
+use sdl2::timer::{delay, get_ticks};
+use std::cmp::{max};
 
 struct Position {
-    x: i32,
-    y: i32,
+    x: f32,
+    y: f32,
 }
 
 struct Movement {
-    dx: i32,
-    dy: i32
+    dx: f32,
+    dy: f32
 }
 
 struct Player {
@@ -31,25 +32,26 @@ struct Background;
 trait Renderable {
     fn draw(&self, renderer:  &sdl2::render::Renderer<Window>);
 
-    fn update(& mut self);
+    fn update(& mut self, timestep: uint);
 }
 
-// TODO: Transfer drawing responsibility to "Drawable" objects
 // TODO: define dy / dx in terms of time
-// TODO: Some keyboard inputs
+// TODO: Some keyboard inputs 
+// TODO: Collision detection
 
 impl Renderable for Player {
     fn draw(&self, renderer:  &sdl2::render::Renderer<Window>) {
         // Set the drawing color to a darker blue.
         let _ = renderer.set_draw_color(sdl2::pixels::RGB(0, 153, 204));
         // Create a smaller centered Rect, filling it in the same dark blue.
-        let l_bat = Rect::new(self.pos.x, self.pos.y, 30, 120);
+        let l_bat = Rect::new(self.pos.x as i32, self.pos.y as i32, 30, 120);
         let _ = match renderer.fill_rect(&l_bat) {
             Ok(_)    => {},
             Err(err) => fail!("failed to draw rect: {}", err) 
         };
     }
-    fn update(& mut self) {
+
+    fn update(& mut self, timestep: uint) {
         self.pos.x += self.mov.dx;
         self.pos.y += self.mov.dy
     }
@@ -59,15 +61,28 @@ impl Renderable for Ball {
     fn draw(&self, renderer: &sdl2::render::Renderer<Window>) {
         // Set the drawing color to a darker blue.
         let _ = renderer.set_draw_color(sdl2::pixels::RGB(0, 153, 204));
-        let ball = Rect::new(self.pos.x, self.pos.y, 20, 20);
+        let ball = Rect::new(self.pos.x as i32, self.pos.y as i32, 20, 20);
         let _ = match renderer.fill_rect(&ball) {
             Ok(_)    => {},
             Err(err) => fail!("failed to draw rect: {}", err) 
         };
     }
-    fn update(& mut self) {
-        self.pos.x += self.mov.dx;
-        self.pos.y += self.mov.dy
+
+    fn update(& mut self, timestep: uint) {
+        // TODO: Hardcoded width is kinda ugly
+        // TODO: Shouldn't bounce off the scoring wall
+        if (self.pos.x + 20.0 >= 640.0 && self.mov.dx > 0.0 )|| 
+           (self.pos.x <= 0.0 && self.mov.dx < 0.0) {
+            self.mov.dx = -self.mov.dx;
+        }
+        if (self.pos.y + 20.0 >= 480.0 && self.mov.dy > 0.0) || 
+           (self.pos.y <= 0.0 && self.mov.dy < 0.0) {
+            self.mov.dy = -self.mov.dy;
+        }
+
+        self.pos.x += self.mov.dx * timestep as f32;
+        self.pos.y += self.mov.dy * timestep as f32;
+
     }
 }
 
@@ -79,14 +94,14 @@ impl Renderable for Background {
         let _ = renderer.clear();
     }
 
-    fn update(& mut self) {
+    fn update(& mut self, timestep: uint) {
 
     }
 }
 
-fn update_all(renderer: &sdl2::render::Renderer<Window>, renderables: &mut [&mut Renderable,..4]) {
+fn update_all(renderer: &sdl2::render::Renderer<Window>, renderables: &mut [&mut Renderable,..4], timestep: uint) {
     for r in renderables.iter_mut() {
-        r.update();
+        r.update(timestep);
         r.draw(renderer);
     }
 }
@@ -95,16 +110,16 @@ fn main() {
     // initialize our position variables
     let mut background = Background;
     let mut l_bat = Player { 
-        pos: Position { x: 0, y: 240-60 },
-        mov: Movement { dx: 0, dy: 0 }
+        pos: Position { x: 0.0, y: 240.0-60.0 },
+        mov: Movement { dx: 0.0, dy: 0.0 }
     };
     let mut r_bat = Player {
-        pos: Position { x: 640-30, y: 240-60 },
-        mov: Movement { dx: 0, dy: 0 }
+        pos: Position { x: 640.0-30.0, y: 240.0-60.0 },
+        mov: Movement { dx: 0.0, dy: 0.0 }
     };
     let mut ball = Ball { 
-        pos: Position { x: 320-10, y: 240-10 }, 
-        mov: Movement { dx: 1, dy: 1 },
+        pos: Position { x: 320.0-10.0, y: 240.0-10.0 }, 
+        mov: Movement { dx: 0.2, dy: 0.2 },
     };
     let renderables: &mut [&mut Renderable,..4] = &mut [
         &mut background as &mut Renderable,
@@ -129,18 +144,21 @@ fn main() {
         Err(err) => fail!("failed to create renderer: {}", err)
     };
 
-    // for r in renderables.iter() {
-    //     r.draw(&renderer)
-    // }
-
     // loop until we receive a QuitEvent
+    let mut last_time = get_ticks();
+    let mut current_time = get_ticks();
+    let target_frame_time: uint = 1000/100; // 100fps = 10ms
     'event : loop {
         match poll_event() {
             QuitEvent(_) => break 'event,
             _            => {
-                update_all(&renderer, renderables);
+                update_all(&renderer, renderables, current_time - last_time);
                 let _ = renderer.present();
-                delay(16);
+                last_time = current_time;
+                current_time = get_ticks();
+                let sleep_time = max(0, target_frame_time as int - (current_time as int - last_time as int));
+                println!("Sleeping for {}", sleep_time);
+                delay(sleep_time as uint / 2); //not sleeping for the whole time gives a smoother result
             }
         }
     }
